4d63efcb779bdf1d740cc379a5db92409d0d4208
orchlonn <ch.orchlon@gmail.com>
2025-02-10 20:03:46 -0800

    bug fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [UsersModule, PlayersModule],
   controllers: [AppController],
   providers: [AppService],
 })
