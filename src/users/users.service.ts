35de81af98ef36877b817afe32cb91723a7316c4
orchlonn <ch.orchlon@gmail.com>
2025-02-19 20:06:30 -0800

    user services are done

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,88 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Orchlon',
+      email: 'orchllnz@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Erica',
+      email: 'erc@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Oscar',
+      email: 'oscarrr.as@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Mile',
+      email: 'mile.gao@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 1,
+      name: 'June',
+      email: 'junae@gmail.com',
+      role: 'ADMIN ',
+    },
+  ];
+
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.filter((user) => user.id === id);
+    return user;
+  }
+
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const userByHighestId = [...this.users].sort((a, b) => (a.id = b.id));
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updatedUser: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updatedUser };
+      }
+      return user;
+    });
+
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+
+    return removedUser;
+  }
+}
